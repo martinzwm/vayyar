fix normalization

data_prep.py
@@ -12,7 +12,7 @@ from torchvision import transforms
 
 #%%
 def firstBatchdataPrep():
-    x, y, occupiedSeat, occupantType, path = importDataOccupancyType("/home/vayyar/FirstBatch")
+    x, y, occupiedSeat, occupantType, path = importDataOccupancyType("/home/vayyar_data/FirstBatch")
     with h5py.File('training_dataset.hdf5', 'w') as f:
         f.create_dataset('x', data=x)
         f.create_dataset('y', data=y)
@@ -34,10 +34,11 @@ class vCabDataSet(Dataset):
             idx = idx.to_list()
         rfImagePath = os.path.join(self.rootDir, str(self.path_label.iloc[idx, 2]) + '.npy')
         imagePower = np.load(rfImagePath)
-        imagePower = np.squeeze(np.sum(imagePower, axis=2)) #TEMPORARY SOLUTION: Squeeze the 3D dataset into 2D 
-        label = self.path_label.iloc[idx, 1]
+        imagePower = np.sum(imagePower, axis=-1, keepdims=True)/imagePower.shape[-1] #TEMPORARY SOLUTION: Squeeze the 3D dataset into 2D 
         if self.transform:
-            imagePower = np.squeeze(self.transform(imagePower))
+            imagePower = self.transform(imagePower)
+            imagePower = np.squeeze(imagePower)
+        label = self.path_label.iloc[idx, 1]
         sample = {'imagePower':imagePower, 'label':label, 'path':rfImagePath}
         return sample
 # %%

first_batch_occupancy_svm.py
@@ -4,9 +4,7 @@
 #%%%
 import numpy as np
 import pandas as pd
-import seaborn as sn
 from matplotlib import style
-style.use("ggplot")
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
@@ -14,7 +12,6 @@ from sklearn.decomposition import PCA
 from sklearn import svm
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.metrics import classification_report, confusion_matrix, multilabel_confusion_matrix, plot_confusion_matrix
-import seaborn
 from sklearn.model_selection import cross_val_score
 import h5py
 from utilities import plot_confusion_matrix, importDataFromMatFiles, importDataOccupancyType, saveData, loadData, seatWiseTransformLabels, scenarioWiseTransformLabels, getConfusionMatrices
@@ -25,8 +22,9 @@ with h5py.File('training_dataset.hdf5', 'r') as f:
     print(f['path'][:])
 #%%print(X.shape)
 print(Y.shape)
-X = np.reshape(X, (X.shape[0],X.shape[1] * X.shape[2] * X.shape[3]))
-print('Xin',X.shape)
+X = np.squeeze(np.sum(X, axis=3))
+# X = np.reshape(X, (X.shape[0],X.shape[1] * X.shape[2] * X.shape[3]))
+print('Xin.shape = ',X.shape)
 print(X.dtype)
 
 # %%