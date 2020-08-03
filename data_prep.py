fixed normalization, verfied

data_prep.py
@@ -4,7 +4,7 @@ import numpy as np
 from utilities import importDataOccupancyType
 import pandas as pd
 import os
-from torch.utils.data import Dataset, DataLoader, Subset
+from torch.utils.data import Dataset, DataLoader, Subset, random_split
 import torch
 from utilities import loadmat
 from torchvision import transforms
@@ -54,62 +54,99 @@ class cropR(object):
         image_power = image_power[:,:,:self.r_dimension]
         return image_power
 # %%
-
-def calculateMeanStd():
+def calculateMeanStd(rootDir):
+    torch.manual_seed(0)
     transform = transforms.Compose([
                 cropR(24),
             ])
-    dataset = vCabDataSet('/home/vayyar_data/processed_vCab_Recordings', transform)
-    batch_size = 256
+    dataset = vCabDataSet(rootDir, transform)
+    train_percent = 0.9
+    validation_percent = 0.05
+    testing_percent = 0.05
+    total_num = len(dataset)
+    training_num = int(train_percent * total_num)
+    validation_num = int(validation_percent * total_num)
+    testing_num = int(total_num - training_num - validation_num)
+    train_set, val_set, test_set = random_split(dataset, [training_num, validation_num, testing_num])
+
+    
+    batch_size = 512
     dataset_loader = DataLoader(
-        dataset,
+        train_set,
         batch_size=batch_size,
         num_workers=8,
-        shuffle=True
+        shuffle=False
     )
     mean = 0.
-    std = 0.
-    count = 0
+    count_mean = 0
     for samples in dataset_loader:
         samples = samples['imagePower']
         mean += samples.mean()
-        std += samples.std()
-        count += 1
+        count_mean += 1
+    mean /= count_mean
 
-    mean /= len(dataset_loader.dataset)/count
-    std /= len(dataset_loader.dataset)/count
+    var = 0
+    count_var = 0
+    for samples in dataset_loader:
+        samples = torch.flatten(samples['imagePower'])
+        var = torch.sum(torch.pow(torch.sub(samples, mean), 2)) / samples.size(0)
+        count_var += 1
+    var /= count_var
+    std = torch.sqrt(var)
     print(f'mean: {mean}, standard deviation: {std}')
-    return mean, std
 
-def verifyNormalization(dataset, image_mean, image_std):
+def verifyNormalization(rootDir, image_mean, image_std):
+    torch.manual_seed(0)
     transform = transforms.Compose([
+            cropR(24),
             transforms.ToTensor(),
             transforms.Normalize(mean=[image_mean],
                                  std=[image_std])
         ])
-    dataset = vCabDataSet('/home/vayyar_data/processed_vCab_Recordings', transform=transform)
-    subset_percent = 0.01
-    subset_num = subset_percent * len(dataset)
-    subset_indices = random.sample(range(0, len(dataset)), subset_num)
-    subset = Subset(dataset, subset_indices)
-    batch_size = 256
+    dataset = vCabDataSet(rootDir, transform=transform)
+    # subset_percent = 0.1
+    # subset_num = int(subset_percent * len(dataset))
+    # print(f'Tested on {subset_num} samples.')
+    # random.seed(1)
+    # subset_indices = random.sample(range(0, len(dataset)), subset_num)
+    # subset = Subset(dataset, subset_indices)
+    
+    train_percent = 0.9
+    validation_percent = 0.05
+    testing_percent = 0.05
+    total_num = len(dataset)
+    training_num = int(train_percent * total_num)
+    validation_num = int(validation_percent * total_num)
+    testing_num = int(total_num - training_num - validation_num)
+    train_set, val_set, test_set = random_split(dataset, [training_num, validation_num, testing_num])
+
+    
+    batch_size = 512
     dataset_loader = DataLoader(
-        subset,
+        train_set,
         batch_size=batch_size,
         num_workers=8,
-        shuffle=True
+        shuffle=False
     )
+
     mean = 0.
-    std = 0.
-    count = 0
+    count_mean = 0
     for samples in dataset_loader:
         samples = samples['imagePower']
         mean += samples.mean()
-        std += samples.std()
-        count += 1
-
-    mean /= len(dataset_loader.dataset)/count
-    std /= len(dataset_loader.dataset)/count
+        count_mean += 1
+    mean /= count_mean
+    
+    var = 0
+    count_var = 0
+    for samples in dataset_loader:
+        samples = torch.flatten(samples['imagePower'])
+        var = torch.sum(torch.pow(torch.sub(samples, mean), 2)) / samples.size(0)
+        count_var += 1
+    var /= count_var
+    std = torch.sqrt(var)
     print(f'mean: {mean}, standard deviation: {std}')
-    return mean, std
 
+
+
+# %%