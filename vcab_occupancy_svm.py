9ac86de92cd9a9e52d344927c260ef36d2d1a151
Jennifer <jennyyu163@yahoo.ca>
2020-08-03 20:04:30 -0400

    uncomment the SVM pytorch approach

--- vcab_occupancy_svm.py
+++ vcab_occupancy_svm.py
@@ -55,136 +55,136 @@ test_loader = DataLoader(
     shuffle=True
 )
 print("finished loaders")
-# #%% Training the SVM
-# import time
-# start = time.time()
-# learning_rate = 0.1  # Learning rate
-# n_epochs = 100  # Number of epochs
+# #%% Training the SVM pytorch approach
+import time
+start = time.time()
+learning_rate = 0.1  # Learning rate
+n_epochs = 100  # Number of epochs
 
-# def make_train_step(model, loss_fn, optimizer):
-#     # Builds function that performs a step in the train loop
-#     def train_step(x, y):
-#         # Clear gradients
-#         optimizer.zero_grad()
-#         # Sets model to TRAIN mode
-#         model.train()
-#         # Makes predictions
-#         y_hat = model(x)
-#         # Computes loss
-#         loss = loss_fn(y, y_hat)  # hinge loss
-#         # Computes gradients
-#         loss.backward()
-#         # Updates parameters and zeroes gradients
-#         optimizer.step()
-#         optimizer.zero_grad()
-#         # Returns the loss
-#         return loss.item()
+def make_train_step(model, loss_fn, optimizer):
+    # Builds function that performs a step in the train loop
+    def train_step(x, y):
+        # Clear gradients
+        optimizer.zero_grad()
+        # Sets model to TRAIN mode
+        model.train()
+        # Makes predictions
+        y_hat = model(x)
+        # Computes loss
+        loss = loss_fn(y, y_hat)  # hinge loss
+        # Computes gradients
+        loss.backward()
+        # Updates parameters and zeroes gradients
+        optimizer.step()
+        optimizer.zero_grad()
+        # Returns the loss
+        return loss.item()
     
-#     # Returns the function that will be called inside the train loop
-#     return train_step
+    # Returns the function that will be called inside the train loop
+    return train_step
 
-# def hinge_loss(y, y_hat):
-#     return torch.mean(torch.clamp(1 - y_hat * y, min=0))
+def hinge_loss(y, y_hat):
+    return torch.mean(torch.clamp(1 - y_hat * y, min=0))
 
-# model = SVM()  # Our model
-# optimizer = optim.Adam(model.parameters(), lr=learning_rate)  # Our optimizer
-# model.train()  # Our model, SVM is a subclass of the nn.Module, so it inherits the train method
-# losses = []
-# val_losses = []
-# train_step = make_train_step(model, hinge_loss, optimizer)
-# train_per_epoch = int(len(train_set) / batch_size)
+model = SVM()  # Our model
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)  # Our optimizer
+model.train()  # Our model, SVM is a subclass of the nn.Module, so it inherits the train method
+losses = []
+val_losses = []
+train_step = make_train_step(model, hinge_loss, optimizer)
+train_per_epoch = int(len(train_set) / batch_size)
 
-# for epoch in range(n_epochs):
-#     sum_loss = 0
-#     sum_val_loss = 0
-#     kbar = pkbar.Kbar(target=train_per_epoch, width=8)
-#     for i, batch in enumerate(train_loader):
-#         #TODO: when have CUDA:
-#         #x_batch = batch['imagePower'].to(device)
-#         #y_batch = batch['label'].to(device)
+for epoch in range(n_epochs):
+    sum_loss = 0
+    sum_val_loss = 0
+    kbar = pkbar.Kbar(target=train_per_epoch, width=8)
+    for i, batch in enumerate(train_loader):
+        #TODO: when have CUDA:
+        #x_batch = batch['imagePower'].to(device)
+        #y_batch = batch['label'].to(device)
 
-#         x_batch = batch['imagePower']
-#         y_batch = batch['label']
+        x_batch = batch['imagePower']
+        y_batch = batch['label']
     
-#         loss = train_step(x_batch, y_batch)
-#         sum_loss += loss
-#         losses.append(loss)
-#         kbar.update(i, values=[("loss", loss)])
+        loss = train_step(x_batch, y_batch)
+        sum_loss += loss
+        losses.append(loss)
+        kbar.update(i, values=[("loss", loss)])
 
-#     print('done training')
-#     with torch.no_grad():
-#         for val_batch in val_loader:
-#             #x_val = x_val.to(device)
-#             #y_val = y_val.to(device)
+    print('done training')
+    with torch.no_grad():
+        for val_batch in val_loader:
+            #x_val = x_val.to(device)
+            #y_val = y_val.to(device)
             
-#             x_val = val_batch['imagePower']
-#             y_val = val_batch['label']
+            x_val = val_batch['imagePower']
+            y_val = val_batch['label']
 
-#             model.eval()
+            model.eval()
 
-#             y_val_pred = model(x_val)
-#             val_loss = hinge_loss(y_val, y_val_pred) 
-#             sum_val_loss += val_loss.item()
-#             val_losses.append(val_loss.item())
+            y_val_pred = model(x_val)
+            val_loss = hinge_loss(y_val, y_val_pred) 
+            sum_val_loss += val_loss.item()
+            val_losses.append(val_loss.item())
             
-#     kbar.add(1, values=[("loss", loss), ("val_loss", val_loss)])
-#     print('done validation')
-#     print("Epoch {}, Loss: {}".format(epoch, sum_loss))
-#     print("Epoch {}, Val Loss: {}".format(epoch, sum_val_loss))
-# print(time.time()-start)
-# #%%
+    kbar.add(1, values=[("loss", loss), ("val_loss", val_loss)])
+    print('done validation')
+    print("Epoch {}, Loss: {}".format(epoch, sum_loss))
+    print("Epoch {}, Val Loss: {}".format(epoch, sum_val_loss))
+print(time.time()-start)
+#%%
 
-# #%%
-# model_path = "/home/vayyar_model/svm_20200727.pt"
-# torch.save(model.state_dict(), model_path)
-# #%%
-# import matplotlib.pyplot as plt
-# fig = plt.figure()
-# ax = plt.axes()
+#%%
+model_path = "/home/vayyar_model/svm_20200727.pt"
+torch.save(model.state_dict(), model_path)
+#%%
+import matplotlib.pyplot as plt
+fig = plt.figure()
+ax = plt.axes()
 
-# x = np.arange(0, len(train_set)*10, batch_size)
-# ax.plot(x, losses[:len(x)])
-# ax.plot(x, val_losses[:len(x)])
-# labels = np.arange(1, 101, 10)
-# plt.xticks(np.arange(min(x), max(x), (max(x))/10))
-# # plt.yticks(np.arange(0, 1.1, 0.1))
-# plt.title('Loss function graph')
-# ax.legend(['loss', 'val_loss'])
-# ax.set_xticklabels(labels)
-# plt.xlabel('epoch')
-# plt.ylabel('loss')
-# plt.show()
+x = np.arange(0, len(train_set)*10, batch_size)
+ax.plot(x, losses[:len(x)])
+ax.plot(x, val_losses[:len(x)])
+labels = np.arange(1, 101, 10)
+plt.xticks(np.arange(min(x), max(x), (max(x))/10))
+# plt.yticks(np.arange(0, 1.1, 0.1))
+plt.title('Loss function graph')
+ax.legend(['loss', 'val_loss'])
+ax.set_xticklabels(labels)
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.show()
 
-# #%%
-# model.load_state_dict(torch.load(model_path))
-# for test_batch in test_loader:
-#     #x_val = x_val.to(device)
-#     #y_val = y_val.to(device)
+#%%
+model.load_state_dict(torch.load(model_path))
+for test_batch in test_loader:
+    #x_val = x_val.to(device)
+    #y_val = y_val.to(device)
     
-#     x_test = test_batch['imagePower']
-#     y_test = test_batch['label']
-#     path = test_batch['path']
+    x_test = test_batch['imagePower']
+    y_test = test_batch['label']
+    path = test_batch['path']
 
-#     model.eval()
-#     y_test_pred = model(x_test)
-#     y_test_pred[y_test_pred < 0.5] = 0
-#     y_test_pred[y_test_pred > 0.5] = 1
-#     y_test_pred.
-#     print(len(y_test_pred[y_test_pred != y_test]))
-#     break
-# #%% this is a one time running cell for calculating te mean standard deviation
-# # mean = 0.
-# # std = 0.
-# # count = 0
-# # for samples in loader:
-# #     samples = samples['imagePower']
-# #     mean += samples.mean()
-# #     std += samples.std()
-# #     count += 1
+    model.eval()
+    y_test_pred = model(x_test)
+    y_test_pred[y_test_pred < 0.5] = 0
+    y_test_pred[y_test_pred > 0.5] = 1
+    y_test_pred.
+    print(len(y_test_pred[y_test_pred != y_test]))
+    break
+#%% this is a one time running cell for calculating te mean standard deviation
+# mean = 0.
+# std = 0.
+# count = 0
+# for samples in loader:
+#     samples = samples['imagePower']
+#     mean += samples.mean()
+#     std += samples.std()
+#     count += 1
 
-# # mean /= len(loader.dataset)/count
-# # std /= len(loader.dataset)/count
-# # print(f'{mean}, {std}')
+# mean /= len(loader.dataset)/count
+# std /= len(loader.dataset)/count
+# print(f'{mean}, {std}')
 
 
 
