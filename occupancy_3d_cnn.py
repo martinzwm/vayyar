e8bfb11caa369c4f6dc44fbc6e374d739049e2f4
Jennifer <jennyyu163@yahoo.ca>
2020-08-09 02:03:51 -0400

    3d cnn working, including training, validation and testing

--- occupancy_3d_cnn.py
+++ occupancy_3d_cnn.py
@@ -17,19 +17,36 @@ from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.optim import *
-from utilities import importDataFromMatFiles, loadData
+from utilities import importDataFromMatFiles, loadData, scenarioWiseTransformLabels
 from models import CNNModel
 from torchvision import transforms
 from data_prep import vCabDataSet, cropR
+import pkbar
+import math
+from torch.utils.tensorboard import SummaryWriter
+#%%
+writer = SummaryWriter()
 
-#%% Import FirstBatch dataset
+#%% Import dataset
+
+#first batch
+# transform = transforms.Compose([
+#             cropR(24),
+#             transforms.ToTensor(),
+#             transforms.Normalize(mean=[1.655461726353112e-06],
+#                                  std=[1.3920989854294221e-05])
+#         ])
+# dataset = vCabDataSet('/home/vayyar_data/processed_FirstBatch', transform)
+
+#vcab_recordings
 transform = transforms.Compose([
             cropR(24),
             transforms.ToTensor(),
-            transforms.Normalize(mean=[1.655461726353112e-06],
-                                 std=[1.3920989854294221e-05])
+            transforms.Normalize(mean=[7.608346462249756],
+                                 std=[6.10889196395874])
         ])
-dataset = vCabDataSet('/home/vayyar_data/processed_FirstBatch', transform)
+dataset = vCabDataSet('/home/vayyar_data/processed_vCab_Recordings', transform)
+
 
 #%% Split training and testing dataset
 train_percent = 0.9
@@ -66,7 +83,8 @@ print("finished loaders")
 #%%
 #Definition of hyperparameters
 start = time.time()
-model_name = "/home/vayyar_model/first_batch_cnn_20200807.pt"
+model_name = "/home/vayyar_model/vCab_Recordings_cnn_20200807.pt"
+# model_name = "/home/vayyar_model/first_batch_cnn_20200807.pt"
 num_classes = 15
 num_epochs = 10
 # Create CNN
@@ -77,31 +95,67 @@ print(model)
 
 # Binary Cross Entropy Loss for MultiLabel Classfication
 error = nn.BCELoss()
-learning_rate = 0.001
+# learning_rate = 0.001 #FirstBatch
+learning_rate = 0.0001 #Vcab_Recordings
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 #%%
 # CNN model training
 loss_list = []
+val_loss_list = []
 iteration_count = 0
+train_per_epoch = math.ceil(len(train_set) / batch_size)
+val_per_epoch = math.ceil(len(val_set) / batch_size)
+
 for epoch in range(num_epochs):
-    for i, (sample) in enumerate(train_loader):
-        images = sample["imagePower"].float()
-        labels = sample["label"].float()
-        train = Variable(images.view(len(images), 1, 29 ,29 ,24))
-        labels = Variable(labels)
+    sum_loss = 0
+    sum_val_loss = 0
+    kbar = pkbar.Kbar(target=train_per_epoch, width=8)
+    for i, sample in enumerate(train_loader):
+        x_train = sample["imagePower"].float()
+        y_train = sample["label"].float()
+        x_train = Variable(x_train.view(len(x_train), 1, 29 ,29 ,24))
+        y_train = Variable(y_train)
         # Clear gradients
         optimizer.zero_grad()
         # Forward propagation
-        outputs = model(train)
+        outputs = model(x_train)
         # Calculate softmax and ross entropy loss
-        loss = error(outputs, labels)
+        loss = error(outputs, y_train)
         # Calculating gradients
         loss.backward()
         # Update parameters
         optimizer.step()
-        loss_list.append(loss.data)
+        # Write to tensorboard
+        writer.add_scalar("Loss/train", loss, epoch)
+        sum_loss += loss.data
+        # loss_list.append(loss.data)
         iteration_count += 1
-        print(f'Epoch {epoch + 1} Iteration {iteration_count}: loss = {loss.data}')
+        kbar.update(i, values=[("loss", loss)])
+        # print(f'Epoch {epoch + 1} Iteration {iteration_count}: loss = {loss.data}')
+    loss_list.append(sum_loss/train_per_epoch)
+    with torch.no_grad():
+        for val_batch in val_loader:
+            
+            x_val = val_batch['imagePower'].float()
+            y_val = val_batch['label'].float()
+            x_val = Variable(x_val.view(len(x_val), 1, 29 ,29 ,24))
+            y_val = Variable(y_val)
+
+            model.eval()
+
+            y_val_pred = model(x_val)
+            val_loss = error(y_val_pred, y_val) 
+            # Write to tensorboard
+            writer.add_scalar("Loss/validation", val_loss, epoch)
+            sum_val_loss += val_loss.data
+            # val_loss_list.append(val_loss.data)
+    val_loss_list.append(sum_val_loss/val_per_epoch)
+    kbar.add(1, values=[("loss", loss), ("val_loss", val_loss)])
+    print('done validation')
+    print("Epoch {}, Loss: {}".format(epoch+1, sum_loss/train_per_epoch))
+    print("Epoch {}, Val Loss: {}".format(epoch+1, sum_val_loss/val_per_epoch))
+writer.flush()
+writer.close()
 torch.save(model.state_dict(), model_name)
 end = time.time()
 print(f'duration = {end - start}s')
@@ -110,23 +164,39 @@ model = CNNModel(num_classes)
 model.load_state_dict(torch.load(model_name))
 model.eval()
 
-predictions = []
-complete_labels = []
-for sample in train_loader:
-    images = sample["imagePower"].float()
-    labels = sample["label"].float()
-    train = Variable(images.view(len(images), 1, 29 ,29 ,64))
+test_per_epoch = math.ceil(len(test_set) / batch_size)
+accuracy = []
+
+f = open('cnn_misclassified_vCab_Recordings.csv', 'w')
+f.write(','.join(['path', 'label_seat', 'predicted_seat', 'label_type', 'predicted_type\n']))
+f.close()
+for sample in test_loader:
+    x_test = sample["imagePower"].float()
+    y_test = sample["label"].float()
+    path = np.array(sample['path'])    
+    train = Variable(x_test.view(len(x_test), 1, 29 ,29 ,24))
     outputs = model(train)
     outputs[outputs < 0.5] = 0
     outputs[outputs > 0.5] = 1
-    predictions.append(outputs)
-    complete_labels.append(labels)
-from sklearn.metrics import classification_report
-from sklearn.metrics import multilabel_confusion_matrix
-cfm = multilabel_confusion_matrix(complete_labels[0].detach().numpy(), predictions[0].detach().numpy())
-report = classification_report(complete_labels[0].detach().numpy(), predictions[0].detach().numpy())
-print(cfm)
-print(report)
-
+    outputs = outputs.detach().numpy()
+    y_test = y_test.detach().numpy()
+    accuracy.append(accuracy_score(y_test, outputs))
+    misclassified_dict = {
+        'path': [],
+        'label_seat': [],
+        'predicted_seat':[],
+        'label_type': [],
+        'predicted_type': []
+    }
+    misclassified_indice = np.where((outputs!=y_test).any(1))
+    if len(misclassified_indice[0]) != 0:
+        misclassified_dict['path'] = list(path[misclassified_indice])
+        misclassified_dict['predicted_seat'], misclassified_dict['predicted_type'] = scenarioWiseTransformLabels(outputs[misclassified_indice])
+        misclassified_dict['label_seat'], misclassified_dict['label_type'] = scenarioWiseTransformLabels(y_test[misclassified_indice])
+        df = pd.DataFrame.from_dict(misclassified_dict)
+        df.to_csv('cnn_misclassified_vCab_Recordings.csv', mode='a', header=False, index=False)
+    
+acc = np.average(np.array(accuracy))
+print(f'Testing accuracy is {acc}.')
 
 # %%
