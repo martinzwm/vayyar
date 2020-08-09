aaf020edfa3242d271fb74fdb7f1d565a9ce3257
Jennifer <jennyyu163@yahoo.ca>
2020-08-09 02:00:30 -0400

    add the data distribution generation function

--- data_prep.py
+++ data_prep.py
@@ -10,6 +10,7 @@ from utilities import loadmat
 from torchvision import transforms
 import random
 import sys
+import matplotlib.pyplot as plt
 
 
 def firstBatchdataPrep():
@@ -54,7 +55,7 @@ class cropR(object):
         assert image_power.shape[-1] >= self.r_dimension, "Cannot crop image to a dimension that is greater than the original dimension, original dimension = {}".format(image_power.shape[-1])
         image_power = image_power[:,:,:self.r_dimension]
         return image_power
-# %%
+
 def calculateMeanStd(rootDir):
     torch.manual_seed(0)
     transform = transforms.Compose([
@@ -130,12 +131,21 @@ def verifyNormalization(rootDir, image_mean, image_std):
     print(f'mean: {mean}, standard deviation: {std}')
 
 # %%
-def findMinAndMax(rootDir):
+def findMinAndMax(rootDir, normalize=False, image_mean=None, image_std=None):
     torch.manual_seed(0)
-    transform = transforms.Compose([
-            cropR(24),
-            transforms.ToTensor(),
-        ])
+    if normalize == False:
+        transform = transforms.Compose([
+                cropR(24),
+                transforms.ToTensor(),
+            ])
+    else:
+        transform = transforms.Compose([
+                cropR(24),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[image_mean],
+                                 std=[image_std])
+            ])
+
     dataset = vCabDataSet(rootDir, transform=transform)
     batch_size = 512
     dataset_loader = DataLoader(
@@ -148,11 +158,62 @@ def findMinAndMax(rootDir):
     min_val = sys.maxsize
     max_val = -sys.maxsize
     for samples in dataset_loader:
-        samples = samples['imagePower']
-        sample_min = torch.min(samples)
-        sample_max = torch.max(samples)
+        imagePower = samples['imagePower']
+        sample_min = torch.min(imagePower)
+        sample_max = torch.max(imagePower)
+        if sample_max >8000:
+            print(samples['path'])
+            break
         if sample_min < min_val: min_val = sample_min
         if sample_max > max_val: max_val = sample_max
     print(f'min value: {min_val}, max value: {max_val}')
+
+# %%
+#FirstBatch min: 0, max: 0.04205722827464342
+#Vcab_recordings min: 0, max: 8178.7294921875
+def plotHistogram(data_min, data_max, num_bins, rootDir, graph_title, normalize=False, image_mean=None, image_std=None):
+    torch.manual_seed(0)
+    if normalize == False:
+        transform = transforms.Compose([
+                cropR(24),
+                transforms.ToTensor(),
+            ])
+    else:
+        transform = transforms.Compose([
+                cropR(24),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[image_mean],
+                                 std=[image_std])
+            ])
+    dataset = vCabDataSet(rootDir, transform=transform)
+    batch_size = 512
+    dataset_loader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        num_workers=8,
+        shuffle=False
+    )
+
+    mybins = np.linspace(data_min, data_max, num_bins)
+    myhist = np.zeros(num_bins-1)
+    for samples in dataset_loader:
+        imagePower = torch.flatten(samples['imagePower']).detach().cpu().numpy()
+        htemp, jnk = np.histogram(imagePower, mybins)
+        myhist += htemp
+    print(myhist)
+    plt.hist(mybins[:-1], bins=mybins, weights=myhist)
+    plt.yscale('log')
+    plt.xlabel('power')
+    plt.ylabel('count')
+    plt.title(graph_title) 
+    plt.show()
+# plotHistogram(0, 0.05, 100, '/home/vayyar_data/processed_FirstBatch_nonthreshold', "FirstBatch Data Distribution No Threshold - 100 Bins")
+# plotHistogram(0, 8200, 100, '/home/vayyar_data/processed_vCab_Recordings_nonthreshold', "vCab_Recordings Data Distribution No Threshold - 100 Bins")
+# plotHistogram(-1, 3025, 100, '/home/vayyar_data/processed_FirstBatch_nonthreshold', "FirstBatch Data Distribution No Threshold (normalized) - 100 Bins", normalize=True,image_mean=1.655461726353112e-06,image_std=1.3920989854294221e-05)
+# plotHistogram(-1, 1, 100, '/home/vayyar_data/processed_FirstBatch_nonthreshold', "FirstBatch Data Distribution No Threshold (normalized)- 100 Bins", normalize=True,image_mean=1.655461726353112e-06,image_std=1.3920989854294221e-05)
+# plotHistogram(-1.5, 1340, 100, '/home/vayyar_data/processed_vCab_Recordings_nonthreshold/', "vCab_Recordings Data Distribution No Threshold (normalized)- 100 Bins", normalize=True,image_mean=7.608346462249756,image_std=6.12775993347168)
+# plotHistogram(-1, 1, 100, '/home/vayyar_data/processed_vCab_Recordings_nonthreshold/', "vCab_Recordings Data Distribution No Threshold (normalized)- 100 Bins", normalize=True,image_mean=7.608346462249756,image_std=6.12775993347168)
+
+
 
 # %%
