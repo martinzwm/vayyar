b3fa58aa8a49ff7d8cfe497ef281d610ad611558
Martin Ma <z74ma@uwaterloo.ca>
2020-11-20 04:08:08 +0000

    normalization

--- lean_data_training.py
+++ lean_data_training.py
@@ -19,9 +19,10 @@ from sklearn.preprocessing import LabelEncoder
 import torch
 from torch.autograd import Variable
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.optim import *
 from utilities import importDataFromMatFiles, loadData, scenarioWiseTransformLabels, getConfusionMatrices, seatWiseTransformLabels, plot_seat_wise_bar, multiclass_metric
-# from torchvision import transforms
+from torchvision import transforms
 from data_prep import rfImageDataSet, cropR
 import math
 from torch.utils.tensorboard import SummaryWriter
@@ -86,6 +87,167 @@ class TrainDataSet(Dataset):
             std += image.std()
         return mean/self.grand_dataset_size, std/self.grand_dataset_size
 
+# Deterministic Auto-Encoder (DAE)
+class DAE(nn.Module):
+
+    # declare layers
+    def __init__(self, in_channel):
+        super(DAE, self).__init__()
+        self.fc1 = nn.Conv3d(in_channel, 64, (3,3,3), padding=(1,1,1))
+        self.fc2 = nn.Conv3d(64, 128, (3,3,3), padding=(1,1,1))
+        self.fc3 = nn.Linear(20, 400)
+        self.fc4 = nn.Linear(400, 784)
+
+    # encoder: one ReLU hidden layer of 400 nodes, one Linear output layer of 20 nodes
+    def encode(self, x):
+        h = F.relu(self.fc1(x))
+        return self.fc2(h)
+
+    # decoder: one ReLU hidden layer of 400 nodes, one sigmoid output layer of 784 nodes
+    def decode(self, z):
+        h3 = F.relu(self.fc3(z))
+        return torch.sigmoid(self.fc4(h3))
+
+    # forward: encoder followed by decoder
+    def forward(self, x):
+        z = self.encode(x.view(-1, 784))
+        return self.decode(z)
+
+
+class VGGNet(nn.Module):
+    def __init__(self, in_channel=1, num_classes=32):
+        super(VGGNet, self).__init__()
+        self.conv_1 = nn.Conv3d(in_channel, 64, (3,3,3), padding=(1,1,1))
+        nn.init.kaiming_normal_(self.conv_1.weight)
+        self.conv_2 = nn.Conv3d(64, 64, (3,3,3), padding=1)
+        nn.init.kaiming_normal_(self.conv_2.weight)
+        self.group_1 = nn.GroupNorm(4,64)
+
+        self.conv_3 = nn.Conv3d(64, 128, (3,3,3), padding=1)
+        nn.init.kaiming_normal_(self.conv_3.weight)
+        self.conv_4 = nn.Conv3d(128, 128, (3,3,3), padding=1)
+        nn.init.kaiming_normal_(self.conv_4.weight)
+        self.conv_5 = nn.Conv3d(128, 128, (3,3,3), padding=1)
+        nn.init.kaiming_normal_(self.conv_5.weight)
+        self.group_2 = nn.GroupNorm(8,128)
+
+        self.conv_6 = nn.Conv3d(128, 256, (3,3,3), padding=1)
+        nn.init.kaiming_normal_(self.conv_6.weight)
+        self.conv_7 = nn.Conv3d(256, 256, (3,3,3), padding=1)
+        nn.init.kaiming_normal_(self.conv_7.weight)
+        self.conv_8 = nn.Conv3d(256, 256, (3,3,3), padding=1)
+        nn.init.kaiming_normal_(self.conv_8.weight)
+        self.group_3 = nn.GroupNorm(16,256)
+
+        self.conv_9 = nn.Conv3d(256, 512, (3,3,3), padding=1)
+        nn.init.kaiming_normal_(self.conv_6.weight)
+        self.conv_10 = nn.Conv3d(512, 512, (3,3,3), padding=1)
+        nn.init.kaiming_normal_(self.conv_7.weight)
+        self.conv_11 = nn.Conv3d(512, 512, (3,3,3), padding=1)
+        nn.init.kaiming_normal_(self.conv_8.weight)
+        self.group_4 = nn.GroupNorm(32,512)
+
+        self.fc_12 = nn.Linear(6912, 4096)
+        self.fc_13 = nn.Linear(4096, num_classes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool3d(kernel_size=2, stride=2)
+        self.drop=nn.Dropout(p=0.5)
+
+    def forward(self, x):
+        x = self.conv_1(x)
+        x = self.relu(x)
+        x = self.group_1(x)
+        x = self.conv_2(x)
+        x = self.relu(x)
+        x = self.group_1(x)
+        x = self.maxpool(x)
+        x = self.drop(x)
+
+        x = self.conv_3(x)
+        x = self.relu(x)
+        x = self.group_2(x)
+        x = self.conv_4(x)
+        x = self.relu(x)
+        x = self.group_2(x)
+        x = self.conv_5(x)
+        x = self.relu(x)
+        x = self.group_2(x)
+        x = self.maxpool(x)
+        x = self.drop(x)
+
+        x = self.conv_6(x)
+        x = self.relu(x)
+        x = self.group_3(x)
+        x = self.conv_7(x)
+        x = self.relu(x)
+        x = self.group_3(x)
+        x = self.conv_8(x)
+        x = self.relu(x)
+        x = self.group_3(x)
+        x = self.maxpool(x)
+        x = self.drop(x)
+
+        # x = self.conv_9(x)
+        # x = self.relu(x)
+        # x = self.group_4(x)
+        # x = self.conv_10(x)
+        # x = self.relu(x)
+        # x = self.group_4(x)
+        # x = self.conv_11(x)
+        # x = self.relu(x)
+        # x = self.group_4(x)
+        # x = self.maxpool(x)
+
+        x = x.view(x.shape[0], -1)
+        x = self.fc_12(x)
+        x = self.relu(x)
+        x = self.drop(x)
+        x = self.fc_13(x)
+        return x
+
+class BasicBlock(nn.Module):
+    def __init__(self, in_planes, planes):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv3d(in_planes, planes, 3, padding = 1)
+        self.bn1 = nn.BatchNorm3d(planes)
+        
+        self.conv2 = nn.Conv3d(planes, planes, 3, padding = 1)
+        self.bn2 = nn.BatchNorm3d(planes)
+        
+        self.shortcut = nn.Sequential()
+        if (in_planes != planes):
+            self.shortcut = nn.Sequential( nn.Conv3d(in_planes, planes, 3, padding = 1),
+                                           nn.BatchNorm3d(planes))
+            
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x) #for the input
+        out = F.relu(out)
+        return out     
+
+class SmallResNet(nn.Module):
+    def __init__(self, in_channel, hidden_channels, num_classes):
+        super(SmallResNet, self).__init__()
+        self.conv = nn.Conv3d(in_channel, hidden_channels[0], 3, padding = 1) #first conv
+        self.bn = nn.BatchNorm3d(hidden_channels[0]) #then batchNorm
+        #now use 3 residual blocks
+        self.res1 = BasicBlock(hidden_channels[0],hidden_channels[1])
+        self.res2 = BasicBlock(hidden_channels[1],hidden_channels[2])
+        self.res3 = BasicBlock(hidden_channels[2],hidden_channels[3])
+        #now do the maxpooling
+        self.maxpool = nn.MaxPool3d(2, 2) 
+        self.fc = nn.Linear(hidden_channels[3] * 3 * 3 * 3 , num_classes) #from maxpooling
+
+    def forward(self, x):
+        out = F.relu(self.bn(self.conv(x)))
+        out = self.res1(out)
+        out = self.res2(out)
+        out = self.res3(out)
+        out = self.maxpool(out)
+        out = self.fc(out.view(out.size(0), -1))
+        return out
+
 class CNNModelRC(nn.Module):
     def __init__(self, num_classes):
         super(CNNModelRC, self).__init__()
@@ -95,8 +257,7 @@ class CNNModelRC(nn.Module):
         self.fc2 = nn.Linear(128, num_classes)
         self.relu = nn.LeakyReLU()
         # self.batch=nn.BatchNorm1d(128)
-        self.drop=nn.Dropout(p=0.50) 
-        self.softmax=nn.LogSoftmax(dim=1)        
+        self.drop=nn.Dropout(p=0.50)   
         
     def _conv_layer_set(self, in_c, out_c):
         conv_layer = nn.Sequential(
@@ -117,29 +278,35 @@ class CNNModelRC(nn.Module):
         # out = self.batch(out)
         out = self.drop(out)
         out = self.fc2(out)
-        out = self.softmax(out)
         return out
 
 def train():
+    # Normalization
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.7263228545248579],
+                                std=[3.6356451880572926])
+    ])
+
     # # For local
     # train_dataset = TrainDataSet([
     #     r'B:\Vayyar_Dataset\small_data\for_martin_vcab\ford1',
     #     r'B:\Vayyar_Dataset\small_data\for_martin_vcab\ford2'
-    # ])
+    # ], transform=transform)
 
     # val_dataset = TrainDataSet([
     #     r'B:\Vayyar_Dataset\small_data\for_martin_vcab\ford1_center'
-    # ])
+    # ], transform=transform)
 
     # For remote
     train_dataset = TrainDataSet([
         '/data/for_martin_vcab/ford1',
         '/data/for_martin_vcab/ford2'
-    ])
+    ], transform)
 
     val_dataset = TrainDataSet([
         '/data/for_martin_vcab/ford1_center'
-    ])
+    ], transform)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -183,13 +350,15 @@ def train():
 
     # Build model
     num_classes = 32
+    # model = VGGNet(in_channel=1, num_classes=num_classes).to(device)
     model = CNNModelRC(num_classes).to(device)
+    # model = SmallResNet(in_channel=1, hidden_channels=[32, 64, 128, 256], num_classes=num_classes).to(device)
     # model = torch.load('lean_cnn.pt').to(device)
     model.train() # mode = train
     print(model)
 
-    error = nn.NLLLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    error = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-3)
 
     # CNN model training
     num_epochs = 200
@@ -221,7 +390,7 @@ def train():
             if device == torch.device('cuda'):
                 sum_train_acc += accuracy_score(outputs.detach().cpu().numpy().argmax(axis=1), y_train.detach().cpu().numpy())
             else:
-                sum_train_acc += accuracy_score(outputs.numpy().argmax(axis=1), y_train.detach().numpy())
+                sum_train_acc += accuracy_score(outputs.detach().numpy().argmax(axis=1), y_train.detach().numpy())
             iteration_count += 1
 
         if device == torch.device('cuda'):
